919e0c4be056bda3c4c03b6548f1468e52e9a283
Pol (SYSADMIT) <pol@sysadmit.com>
2023-05-24 21:48:04 +0200

    CamposAnidados funcionando

---
+++ 1.js
@@ -0,0 +1,32 @@
+var Campos = {
+    L: {
+        ImporteVivo: [ImporteVivoL1, ImporteVivoL2, ImporteVivoL3],
+        PorcentajeTipoInteres: [PorcentajeTipoInteresL1, PorcentajeTipoInteresL2, PorcentajeTipoInteresL3],
+        PorcentajeRetencion: [PorcentajeRetencionL1, PorcentajeRetencionL2, PorcentajeRetencionL3],
+        DiasTranscurridos: [DiasTranscurridosL1, DiasTranscurridosL2, DiasTranscurridosL3],
+        InteresesEuros: [InteresesEurosL1, InteresesEurosL2, InteresesEurosL3],
+        FechaLiquidacion: [FechaLiquidacionL1, FechaLiquidacionL2, FechaLiquidacionL3],
+        RetencionEuros: [RetencionEurosL1, RetencionEurosL2, RetencionEurosL3],
+        MasIntereses: [MasInteresesL1, MasInteresesL2, MasInteresesL3],
+        MenosRetencion: [MenosRetencionL1, MenosRetencionL2, MenosRetencionL3],
+        Resultado: [ResultadoL1, ResultadoL2, ResultadoL3]
+    },
+    R: {
+        ImporteReintegro: [ImporteReintegroR1, ImporteReintegroR2, ImporteReintegroR3],
+        PorcentajePenalizacion: [PorcentajePenalizacionR1, PorcentajePenalizacionR2, PorcentajePenalizacionR3],
+        DiasTranscurridos: [DiasTranscurridosR1, DiasTranscurridosR2, DiasTranscurridosR3],
+        DiasTranscurridosDesdeAnteriorLiquidacion: [DiasTranscurridosDesdeAnteriorLiquidacionR1, DiasTranscurridosDesdeAnteriorLiquidacionR2, DiasTranscurridosDesdeAnteriorLiquidacionR3],
+        InteresesReintegor: [InteresesReintegorR1, InteresesReintegorR2, InteresesReintegorR3],
+        PenalizacionEuros: [PenalizacionEurosR1, PenalizacionEurosR2, PenalizacionEurosR3],
+        MaximoInteresesBrutos: [MaximoInteresesBrutosR1, MaximoInteresesBrutosR2, MaximoInteresesBrutosR3],
+        PenalizacionAplicar: [PenalizacionAplicarR1, PenalizacionAplicarR2, PenalizacionAplicarR3],
+        PorcentajeRetencion: [PorcentajeRetencionR1, PorcentajeRetencionR2, PorcentajeRetencionR3],
+        RetencionEuros: [RetencionEurosR1, RetencionEurosR2, RetencionEurosR3],
+        MasCapital: [MasCapitalR1, MasCapitalR2, MasCapitalR3],
+        MasIntereses: [MasInteresesR1, MasInteresesR2, MasInteresesR3],
+        MenosRetencion: [MenosRetencionR1, MenosRetencionR2, MenosRetencionR3],
+        MenosPenalizacion: [MenosPenalicacionR1, MenosPenalicacionR2, MenosPenalicacionR3],
+        Resultado: [ResultadoR1, ResultadoR2, ResultadoR3]
+    }
+}
+
